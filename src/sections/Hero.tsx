8b3bb5a5415dd11b01e32ca4c2ab09f48ecd9f83
elPaleniozord <jacekwalasik89@gmail.com>
2020-08-13 21:24:09 +0200

    add fonts

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Section from './Section'
+import styled from 'styled-components'
+
+const H2 = styled.h2`
+  font-family: 'Lato';
+`
 
 const Hero = () => {
   return (
     <Section id='hero'>
       <h1>JACEK WALASIK</h1>
-      <h2>Web Developer</h2>
+      <H2>Web Developer</H2>
       <p>in training</p>
-      
+
       <div>
         ICONS
         <link rel="stylesheet" href=""/>
--- src/theme/GlobalStyle.tsx
+++ src/theme/GlobalStyle.tsx
@@ -2,8 +2,18 @@ import { format } from "path";
 import {createGlobalStyle} from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Titillium Web';
+    font-style: normal;
+    src: url('./titillium-web.woff2') format('woff2')
+  }
+  @font-face {
+    font-family: 'Lato';
+    src: url('./lato.woff2') format('woff2');
+  }
   body {
     background-color: none;
+    font-family: 'Titillium Web'
   }
 `
 
